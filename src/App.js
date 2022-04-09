cc6a342348d29b04dee4a807f9267096298539f6
Raju <raju.burra3@gmail.com>
2022-04-09 07:19:30 +0530

    top mobiles brands in world

--- src/App.js
+++ src/App.js
@@ -4,20 +4,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+              <h1>Top 10 Mobile Brands in World</h1>
+        
+        <img className = "images" src = "https://www.edudwar.com/wp-content/uploads/2021/09/top-mobile-phone-companies-1280x720.jpg" alt = ""></img>
+ 
+        <p>As we are in the 21st century and the mobiles and cell phoneshave become regarded as the century icon. Mobile phones have come a long way, they are the most used devices in the world and the growth of the mobile industry has been amazing and this has been an astonishing achievement for the industry. The growth of the mobile industry has opened doors for many entrepreneurs; however, startups must also hold with the dynamical trends, purchaser behavior, and strategies of companies like Google, Apple, Microsoft in the corporate field. If a startup knows this Enhancement, then this would help them grasp the great opportunity while lessening risk from potential issues.</p>
+
+        <div className = "row">
+ 
+        <div className = "mobile">
+        <h2>SAMSUNG</h2>
+        <img className = "images" src = "https://i.pinimg.com/originals/66/23/c7/6623c7a1ecffd8971c3baf9dcb148ae3.jpg" alt = ""></img>
+        <p>Samsung is a big name in phone companies and has emerged as one of the top mobile brands in the world. It is a South Korean company founded on 1st March 1938. It is one of the most trusted brands in the world because of the top-notch facilities provided by the company and its long-lasting smartphones.</p>
+        </div>
+        
+        <div className = "mobile">
+        <h2>APPLE</h2>
+        <img className = "images" src = "https://static01.nyt.com/images/2020/04/16/business/16apple-new/15apple-new-articleLarge.jpg?quality=75&auto=webp&disable=upscale" alt = ""></img>
+        <p>It is one of the most premium mobile brands in the world. It is an American multinational technology company and is one of the biggest tech companies in the world. It was founded on April 1st, 1976 by Steve Jobs, Steve Wozniak, Ronald Wayne. The top smartphone hand of this company includes Apple iPhone 11, iPhone 11 Pro, iPhone X, iPhone XS Max, iPhone XR.</p>
+        </div>
+        
+        <div className = "mobile">
+        <h2>HUAWEI</h2>
+        <img className = "images" src = "https://consumer.huawei.com/content/dam/huawei-cbg-site/common/mkt/list-image/phones/p40/p40-silver.png" alt = ""></img>
+        <p>It’s one of the wide-ranging smartphone brands in the world. It is a Chinese multinational company founded in 1987 by Ren Zhengfei. Popular handsets launched by this company are Huawei P30 Pro, Huawei Mate 20 Pro, Huawei P30, etc.</p>
+        </div>
+        </div>
+      
     </div>
   );
 }
